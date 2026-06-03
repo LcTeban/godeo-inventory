5cd68987fa12af076c593def9c48cca60185624a
LcTeban <nuevotebanlc@gmail.com>
2026-06-03 21:26:34 +0200

    Add useLocation import to Layout component

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { Outlet, Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { useTheme } from '../context/ThemeContext';
 import {
